df2c821bf8dde3f9342659f53726ec46c586ec40
wesley perez <wesley.m.perez@gmail.com>
2019-12-26 14:03:03 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -1,6 +1,6 @@
 def using_push(array,string)
- games = ["mario","call of duty","wwe"]
- games.push("violet")
+ colors = ["blue","red","green"]
+ colors.push("violet")
  end
 
 def using_unshift(array,string)
@@ -9,37 +9,40 @@ def using_unshift(array,string)
 end
 
 def using_pop(array)
-  contients = ["North America", "South America", "Europe", "Africa", "Asia", "Antarctica"]
+  contients = ["North America", "South America", "Europe","Australia", "Africa", "Asia", "Antarctica"]
   contients.pop
+
 
 end
 
 def pop_with_args(array)
- small_dogs = ["Chihuahua","Shiba Inu"]
- dog_breeds = [small_dogs]
-  dog_breeds.pop
-end
+ small_dogs = ["husky","Chihuahua","Shiba Inu"]
+ small_dogs.pop(2)
+  end
+
 
 def using_shift(array)
   my_favorite_cities = ["Lagos","jersey","bayonne","manhattan","baltimore","toms river"]
-  my_favorite_cities.shift
+  my_favorite_cities.shift!
+
 end
 
 def shift_with_args(array)
-  games = ["mario","call of duty",]
-  games.shift("mario","call of duty")
+  games = ["need for speed","mario","call of duty",]
+  games.shift(2)
+
 end
 
-def using_concat(array1,array2)
-all_my_favs = ["raindrops on roses", "whiskers on kittens", "sports cars", "flatiron school"]
-  characters = ["luigi","golem"]
-  games.concat(characters)
+def using_concat(array,array)
+all_my_favs = ["raindrops on roses", "whiskers on kittens", ]
+  characters = ["sports cars", "flatiron school"]
+  all_my_favs.concat(characters)
 end
 
 def using_insert(array,element)
   games = ["mario","call of duty","wwe","need for speed"]
-  new = "wow"
-  games.insert(new)
+  games.insert("2k19")
+  p games 
 end
 
 def using_uniq(array)
@@ -50,14 +53,16 @@ end
 def using_flatten(array)
   characters = ["Drums","Flute"]
   games = ["Saxophone", "Piano", "Trumpet", "Violin",characters]
-  games.flatten(characters)
+  games.flatten
 end
 
 def using_delete(array,string)
   instructors = ["Josh", "Steven", "Sophie", "Steven", "Amanda", "Steven"]
-  instructors.include?("Steven")
-  while true do
-    instructors.delete("Steven")
+  while instructors.include?("Steven") do
+    instructors.delete
+    puts instructors
+  end
+
 end
 
 def using_delete_at(array,integer)
